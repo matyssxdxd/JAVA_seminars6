812f773ca8761b09707aefc4144fd769184286f3
matyssxd <matissp1302@gmail.com>
2024-04-29 11:07:40 +0300

    first commit

---
+++ src/main/java/lv/venta/seminars6/JavaSeminars6Application.java
@@ -0,0 +1,13 @@
+package lv.venta.seminars6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSeminars6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSeminars6Application.class, args);
+	}
+
+}
